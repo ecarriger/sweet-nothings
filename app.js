fe51d7374868c35a3108f2b4b41acc2a112fdf0f
Ethan Carriger <ethancarriger@gmail.com>
2024-08-19 14:16:12 -0400

    Add sample message building blocks

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 //Sweet Nothings App
 console.log("Sweet Nothings App");
+
+//Message building blocks
+const nickname = ["Baby", "My Love", "Honey", "Honey Bear", "Dearest", "Babe", "Sweetie", "Boo"];
+const comparison = ["you are better than", "I love you more than", "you are more beautiful than", "you are as sweet as", "I need you more than"];
+const goodThing = ["sun", "ice cream", "world", "cherry on top", "Sistine chapel", "happily ever after", "Mona Lisa"];
